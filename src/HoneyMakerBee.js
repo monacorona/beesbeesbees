a0c9fb0861d75a54692ac8289170caa8bdf07eab
monacorona <monacoronababy@gmail.com>
2017-02-13 15:28:22 -0800

    Finished everythang :)

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,25 @@
 var HoneyMakerBee = function() {
+// call links subclass to superclass
+  Bee.call(this);
+  //own property
+  this.honeyPot = 0;
+  //changes former age (from Bee)
+  this.age = 10;
+  this.job = "make honey";
 };
+
+//creates a Bee prototype with the name of HoneyMakerBee
+//constructor makes the protorype HoneyMakerBee's own
+HoneyMakerBee.prototype = Object.create(Bee.prototype);
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
+
+//create two methods, makeHoney and giveHoney
+
+HoneyMakerBee.prototype.makeHoney = function(){
+  return this.honeyPot++;
+}
+
+HoneyMakerBee.prototype.giveHoney = function(){
+  return this.honeyPot--;
+}
 
